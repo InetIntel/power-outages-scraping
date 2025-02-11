2ddc64eaa5f5bcec6b0c9f62e9cfb39d96ffcae7
Peng Peng24 Chen <chenpeng24@lenovo.com>
2025-02-11 13:24:42 +0800

    collect every field of Vinnytsia

--- Ukraine/Vinnytsia.py
+++ Ukraine/Vinnytsia.py
@@ -11,10 +11,9 @@ class PlannedDisconnectionSpider(scrapy.Spider):
     allowed_domains = ["voe.com.ua"]
 
     # Start URL for the spider
-    years = ['2023', '2024', '2025']
-    months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
-    regions = ['23', '25', '26', '27', '29', '30', '31', '32', '33' ,'35', '36', '37', '38', '39'
-               ,'41', '42', '43', '45', '46', '47', '48', '50', '51', '52', '53', '55', '56', '57']
+    years = ['2024']
+    months = ['01', '02']
+    regions = ['23', '25']
 
     start_urls = []
 
@@ -26,18 +25,30 @@ class PlannedDisconnectionSpider(scrapy.Spider):
     def parse(self, response):
         # Extract relevant data from the page
         # Adjust the selectors based on the structure of the webpage
-        planned_disconnections = response.xpath('//table')
+        planned_disconnections = response.xpath('//tr[@class="row"]')
 
         for disconnection in planned_disconnections:
+            planned_end_time = disconnection.xpath('.//td[@class="accend_plan"]/text()').get()
+            actual_end_time = disconnection.xpath('.//td[@class="accend_fact"]/text()').get()
+            start_time = disconnection.xpath('.//td[@class="accbegin"]/text()').get()
+            disconnection_type = disconnection.xpath('.//td[@class="acctype"]/text()').get()
+            city_list = disconnection.xpath('.//td[@class="city_list"]/text()').get()
+            street_list = disconnection.xpath('.//td[@class="addresses"]/text()').get()
+            status = disconnection.xpath('.//td[@class="status"]/text()').get()
+            inform_time = disconnection.xpath('.//td[@class="dtupdate"]/text()').get()
             yield {
-                'region': disconnection.xpath('.//h2/text()').get(),
-                'description': disconnection.xpath('.//p/text()').get(),
-                'date': disconnection.xpath('.//span[@class="date"]/text()').get(),
-                'location': disconnection.xpath('.//span[@class="location"]/text()').get(),
+                'planned_end_time': planned_end_time,
+                'actual_end_time': actual_end_time,
+                'start_time': start_time,
+                'city_list': city_list,
+                'street_list': street_list,
+                'disconnection_type': disconnection_type,
+                'status': status,
+                'inform_time': inform_time
             }
 
         # If there are pagination links, follow them
-        next_page = response.xpath('//a[@class="next-page"]/@href').get()
+        next_page = response.xpath('//a[@rel="next-page"]/@href').get()
         if next_page:
             yield response.follow(next_page, self.parse)
 
