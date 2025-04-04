d22dac887ed71fd252b05439aecd64ef0cee918a
Peng Peng24 Chen <chenpeng24@lenovo.com>
2025-04-04 15:24:36 +0800

    change the top dir name to 'power-outages-data'

---
+++ ukraine/zhytomyr/crawler.py
@@ -0,0 +1,35 @@
+# Import necessary libraries
+import datetime
+from utils import raw_file, mk_dir
+import requests
+
+
+# Name of the spider
+name = "planned_disconnection_zhytomyr"
+now = datetime.datetime.now()
+one_day_later = now + datetime.timedelta(days=1)
+formatted_start_date = str(int(now.timestamp()))
+formatted_end_date = str(int(one_day_later.timestamp()))
+
+
+def main():
+        url = "https://www.ztoe.com.ua/unhooking-search.php"
+
+
+        payload = {
+            "rem_id": "0",
+            "naspunkt_id": "0",
+            "vulica_id": "0",
+            "all": "%EF%EE%EA%E0%E7%E0%F2%E8 %F9%E5 1264 %E7%E0%EF%E8%F1%B3%E2"
+        }
+
+        response = requests.post(url, data=payload)
+
+        current_html_file = raw_file.replace(".json", ".html")
+        with open(current_html_file, 'w') as f:
+            f.write(response.text)
+
+
+if __name__ == "__main__":
+    mk_dir()
+    main()
