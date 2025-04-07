442eac9d3838b84b2ae2147b4437fc4c8579bd95
Peng Peng24 Chen <chenpeng24@lenovo.com>
2025-04-07 21:54:08 +0800

    make craw_ukraine.sh

---
+++ ukraine/craw_ukraine.sh
@@ -0,0 +1,17 @@
+Python3 ./cherkasy/crawler.py
+Python3 ./cherkasy/post_processor.py
+
+Python3 ./khmelnytsky/crawler.py
+Python3 ./khmelnytsky/post_processor.py
+
+Python3 ./mykolaiv/crawler.py
+Python3 ./mykolaiv/post_processor.py
+
+Python3 ./sumy/crawler.py
+Python3 ./sumy/post_processor.py
+
+Python3 ./vinnytsia/crawler.py
+Python3 ./vinnytsia/post_processor.py
+
+Python3 ./zhytomyr/crawler.py
+Python3 ./lviv/crawler.py
