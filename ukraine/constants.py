6e8cee7d1466002ceb019438de37acff8e9b05e6
Peng Peng24 Chen <chenpeng24@lenovo.com>
2025-04-29 21:15:09 +0800

    change root_dir of constants

--- ukraine/constants.py
+++ ukraine/constants.py
@@ -3,4 +3,4 @@ from datetime import datetime
 current_month = datetime.now().month
 current_year = datetime.now().year
 current_date = str(datetime.now().date())
-root_dir = '/Users/chenpeng/PycharmProjects'
+root_dir = ''
