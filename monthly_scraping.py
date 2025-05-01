fd52602091cda2380597f1700686c2a2b66f2008
Peng Peng24 Chen <chenpeng24@lenovo.com>
2025-05-01 16:55:10 +0800

    fix monthly_scraping.py

--- monthly_scraping.py
+++ monthly_scraping.py
@@ -1,5 +1,10 @@
-from india.posoco.posoco import Posoco
-from india.mahavitaran.mahavitaran import Mahavitaran
+from scrapy.crawler import CrawlerProcess
+
+# from india.posoco.posoco import Posoco
+# from india.mahavitaran.mahavitaran import Mahavitaran
+from ukraine.vinnytsia.crawler import VinnytsiaSpider
+from ukraine.vinnytsia.utils import mk_dir as mk_dir_for_vinnytsia
+from ukraine.vinnytsia.post_processor import post_process_vinnytsia
 
 
 def scrape():
@@ -16,11 +21,18 @@ def scrape():
     except Exception as e:
         print("Failed to scrape outage data from POSOCO.")
 
-
+    # Ukraine Vinnytsia
+    process = CrawlerProcess()
     try:
-        run_vinnytsia_spider()
+        mk_dir_for_vinnytsia()
+        process.crawl(VinnytsiaSpider)
+        process.start()
     except Exception as e:
-        print("Failed to scrape outage data of Ukraine")
+        print("Failed to scrape outage data of Ukraine Vinnytsia")
+    try:
+        post_process_vinnytsia()
+    except Exception as e:
+        print("Failed to cope with Ukraine Vinnytsia")
 
 
 if __name__ == "__main__":
