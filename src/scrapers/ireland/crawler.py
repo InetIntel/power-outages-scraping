083880e3757b2123efea60ccf05ee45073abbb4b
cbarkey <christopherdbarkey@gmail.com>
2025-11-07 09:24:47 -0500

    Added Notes and the ability to loop through all the data
    Added a notes section that details the data obtained. Added a loop to the api call so it obtains all the data if there are multiple pages.

--- src/scrapers/ireland/crawler.py
+++ src/scrapers/ireland/crawler.py
@@ -15,7 +15,7 @@ class Ireland_API():
                         "Portlaoise", "Roscrea", "Sligo", "Thurles", "Tralee", "Tuam",
                         "Tullamore", "Waterford"]
         # self.regions = ["Dublin%20North"]
-        self.name_csv = "/output/ireland_power_outage.csv"
+        self.name_csv = "output/ireland_power_outage.csv"
     def retrieve_data(self):
         # Retrieve up to 1000 pieces of data from each region
         # This will need to be changed to stop if less data is given
@@ -36,19 +36,42 @@ class Ireland_API():
         }
         # Retrieve the data region by region and add it to a master data structure
 
+        results = 100
+
         for region in self.regions:
-            url = f"https://api.esb.ie/esbn/powercheck/v1.0/plannergroups/{region}/outages"
-            # Retrieve the outage info for that region
-            resp = requests.get(url, headers=headers, params=params)
-            data = resp.json()
-            # Check if there is any data
-            if "outageDetail" not in data:
-                continue
-            # Temporarily add the data to a pandas dataframe
-            page_df = pd.DataFrame(data['outageDetail'])
-            # write the df to a csv file
-            page_df.to_csv(self.name_csv, mode='a', index=False, header=not os.path.exists(self.name_csv))
-            time.sleep(random.uniform(0.5, 1.5))
+            page = 1
+            while True:
+                params = {
+                    "page": page,
+                    "results": results,
+                    "sort": 3,
+                    "order": 1,
+                    "filter": "",
+                    "rnd": "0.123456"
+                }
+                url = f"https://api.esb.ie/esbn/powercheck/v1.0/plannergroups/{region}/outages"
+                # Retrieve the outage info for that region
+                resp = requests.get(url, headers=headers, params=params)
+
+                # You come to the last page if there is a 404 status
+                if resp.status_code == 404:
+                    break
+
+                data = resp.json()
+                
+                # Check if there is any data
+                if "outageDetail" not in data:
+                    continue
+                # Temporarily add the data to a pandas dataframe
+                page_df = pd.DataFrame(data['outageDetail'])
+                # write the df to a csv file
+                page_df.to_csv(self.name_csv, mode='a', index=False, header=not os.path.exists(self.name_csv))
+                page += 1
+
+                time.sleep(random.uniform(0.5, 1.5))
+                # You know you are on the last page of data if it gives you less than the requested results
+                if len(page_df) < results:
+                    break
 
             # Debug statements for the Docker container
             # print(f"Calling the API for {region}")
