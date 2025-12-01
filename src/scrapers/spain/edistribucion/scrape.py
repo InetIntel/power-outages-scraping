75a5d77dd3db54fcb2160976116eef5ffd26fb59
Henry King <henry.king0807@gmail.com>
2025-11-30 22:35:51 -0800

    connected spain/edistribucion to minio
    - connected spain/edistribucion scraper to minio and added data processing
    - added notes.md file to spain/naturgy scraper

--- src/scrapers/spain/edistribucion/scrape.py
+++ src/scrapers/spain/edistribucion/scrape.py
@@ -42,62 +42,6 @@ def get_data():
 
     return all_features
 
-def upload_raw(raw_data: List[Dict[str, Any]]):
-    """
-    Upload raw data json to minio storage.
-    """
-    now = datetime.now(timezone.utc)
-    current_year = f"{now.year}"
-    current_month = f"{now.month:02d}"
-    current_day = f"{now.day:02d}"
-    current_hour = f"{now.hour:02d}"
-    current_minute = f"{now.minute:02d}"
-    current_second = f"{now.second:02d}"
-
-    # first save raw data locally to container as json
-    filename = f'power_outages.ES.edistribucion.raw.{current_year}-{current_month}-{current_day}-{current_hour}-{current_minute}-{current_second}.json'
-    with open(filename, 'w') as f:
-        json.dump(raw_data, f, ensure_ascii=False, indent=4)
-
-    # now upload json to minio
-    uploader = Uploader('spain')
-    local_path = os.path.join(
-        os.path.dirname(os.path.abspath(__file__)),
-        filename
-    )
-    s3_path = f"edistribucioin/raw/{current_year}/{current_month}/{filename}"
-    uploader.upload_file(local_path, s3_path)
-
-def save_json(data):
-    """
-    Save current raw outage data to local
-    docker container as a json file.
-    """
-    # Directory where the script is located
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-
-    # Folder to store data
-    data_dir = os.path.join(script_dir, "data")
-
-    # Ensure the directory exists
-    os.makedirs(data_dir, exist_ok=True)
-
-    # Build the filename with timestamp
-    now = datetime.now()
-    current_year = f"{now.year}"
-    current_month = f"{now.month:02d}"
-    current_day = f"{now.day:02d}"
-    file_name = f'power_outages.ES.edistribucion.raw.{current_year}-{current_month}-{current_day}.json'
-    file_path = os.path.join(data_dir, file_name)
-
-    # Save the file
-    try:
-        with open(file_path, "w", encoding="utf-8") as f:
-            json.dump(data, f, ensure_ascii=False, indent=2)
-        print(f"Saved JSON to: {file_path}")
-    except Exception as e:
-        print(f"Failed to save JSON: {e}")
-
 def get_pre_existing_outages():
     """
     Get json of pre-existing outages data in "current_outages" folder in minio bucket.
@@ -127,7 +71,7 @@ def update_current_outages(pre_existing_outages, new_data):
         o["attributes"]["objectid1"]: o for o in pre_existing_outages if "objectid1" in o["attributes"]
     }
 
-    # collect new "current_outages" as we compare to new raw data
+    # collect updated "current_outage" data
     updated_current_outages: List[Dict[str, Any]] = []
 
     # keep track of ongoing outage ids to know if any "current" outages are resolved
@@ -147,19 +91,25 @@ def update_current_outages(pre_existing_outages, new_data):
         existing = current_by_id.get(outage_id)
         if existing is None:
             outage["ioda_status"] = "in_progress"
-            if not outage['attributes']['interruption_date']:
+            """if not outage['attributes']['interruption_date']:
                 timestamp_ms = int(time.time() * 1000)
-                outage['attributes']['interruption_date'] = timestamp_ms
+                outage['attributes']['interruption_date'] = timestamp_ms"""
+            time_stamp_ms = int(time.time() * 1000)
+            outage['ioda_detection_date'] = time_stamp_ms
+            outage['ioda_update_date'] = time_stamp_ms
+
             updated_current_outages.append(outage)
             continue # move on to next outage
 
         # else if outage is present already, update "last_edited_date" attribute, take max of
-        # "CLIENTES_AFECTADOS", add to "updated_current_outages" and move on to next outage
+        # "affected_client", add to "updated_current_outages" and move on to next outage
         updated_outage = dict(existing)
         updated_outage["attributes"] = dict(existing["attributes"])
         updated_outage["attributes"]["affected_client"] = max(0, existing["attributes"]["affected_client"], outage["attributes"]["affected_client"])
         if "last_edited_date" in outage["attributes"]:
             updated_outage["attributes"]["last_edited_date"] = outage["attributes"]["last_edited_date"]
+        time_stamp_ms = int(time.time() * 1000)
+        updated_outage['ioda_update_date'] = time_stamp_ms
         updated_current_outages.append(updated_outage)
 
     # all "current_outages" that did not appear in new data, change "ioda_status" to "resolved"
@@ -167,26 +117,191 @@ def update_current_outages(pre_existing_outages, new_data):
         if outage["attributes"]["objectid1"] not in ongoing_outage_ids:
             completed_outage = dict(outage)
             completed_outage["ioda_status"] = "resolved"
-            if not completed_outage['attributes']['last_edited_date']:
+            """if not completed_outage['attributes']['last_edited_date']:
                 timestamp_ms = int(time.time() * 1000)
-                completed_outage['attributes']['last_edited_date'] = timestamp_ms
+                completed_outage['attributes']['last_edited_date'] = timestamp_ms"""
+            time_stamp_ms = int(time.time() * 1000)
+            completed_outage['ioda_update_date'] = time_stamp_ms
             updated_current_outages.append(completed_outage)
 
     return updated_current_outages
 
+def get_pre_existing_raw_outages(current_datetime, bucket_name, provider_name):
+    """
+    Get json of pre-existing raw outages data for today from minio.
+    If no pre-existing data, return [].
+    """
+    # create uploader object
+    uploader = Uploader(bucket_name)
+    file_name = f"power_outages.ES.{provider_name}.raw.{current_datetime['current_year']}-{current_datetime['current_month']}-{current_datetime['current_day']}.json"
+
+    # specify local path to download file
+    local_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), file_name
+    )
+
+    # Get data if file exists, else create list to store new data to upload
+    try:
+        s3_path = f"{provider_name}/raw/{current_datetime['current_year']}/{current_datetime['current_month']}/{file_name}"
+        uploader.download_file(s3_path, local_path)
+        with open(file_name, 'r') as file:
+            pre_existing_raw_outages = json.load(file)
+        if not pre_existing_raw_outages:
+            pre_existing_raw_outages = []
+    except FileNotFoundError as e:
+        pre_existing_raw_outages = []
+
+    # return current raw data to append to
+    return pre_existing_raw_outages
+
+def update_raw_data(pre_existing_raw_outage_data, new_raw_outage_data):
+    """
+    Takes in today's pre-existing raw data from minio and new raw data that just returned from api.
+    Returns a new updated raw data file including previously stored and new raw data.
+    """
+
+    # store all object id's, pre-existing and new
+    all_ids = set()
+
+    # index the old raw data by object id
+    indexed_old_raw_outages = {}
+    for outage in pre_existing_raw_outage_data:
+        id = outage["attributes"]["objectid1"]
+        indexed_old_raw_outages[id] = outage
+        all_ids.add(id)
+
+    # index the new raw data by object id
+    indexed_new_raw_outages = {}
+    for outage in new_raw_outage_data:
+        id = outage["attributes"]["objectid1"]
+        indexed_new_raw_outages[id] = outage
+        all_ids.add(id)
+
+    # list to store new updated raw outage data
+    updated_raw_outage_data = []
+
+    # iterate through each outage by id
+    for id in all_ids:
+
+        # if outage just started
+        if id in indexed_new_raw_outages.keys() and id not in indexed_old_raw_outages.keys():
+            outage = indexed_new_raw_outages[id]
+            outage['outage_ended_today'] = False
+            updated_raw_outage_data.append(outage)
+
+        # if outage just ended
+        elif id in indexed_old_raw_outages.keys() and id not in indexed_new_raw_outages.keys():
+            outage = indexed_old_raw_outages[id]
+            outage['outage_ended_today'] = True
+            updated_raw_outage_data.append(outage)
+
+        # if outage is ongoing
+        else:
+            previous_outage_data = indexed_old_raw_outages[id]
+            new_outage_data = indexed_new_raw_outages[id]
+            num_people_affected = max(0, previous_outage_data['attributes']['affected_client'], new_outage_data['attributes']['affected_client'])
+            outage = new_outage_data
+            outage['attributes']['affected_client'] = num_people_affected
+            outage['outage_ended_today'] = False
+            updated_raw_outage_data.append(outage)
+
+    return updated_raw_outage_data
+
+def upload_raw(raw_data: List[Dict[str, Any]], current_datetime, bucket_name, provider_name):
+    """
+    Upload raw data json from local docker container to minio storage.
+    """
+    # get date for file naming
+    current_year = current_datetime['current_year']
+    current_month = current_datetime['current_month']
+    current_day = current_datetime['current_day']
+
+    # save raw data locally to container as json
+    filename = f'power_outages.ES.{provider_name}.raw.{current_year}-{current_month}-{current_day}.json'
+    with open(filename, 'w') as f:
+        json.dump(raw_data, f, ensure_ascii=False, indent=4)
+
+    # now upload json to minio
+    uploader = Uploader(f'{bucket_name}')
+    local_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        filename
+    )
+    s3_path = f"{provider_name}/raw/{current_year}/{current_month}/{filename}"
+    uploader.upload_file(local_path, s3_path)
+
+def get_pre_existing_current_outages(bucket_name, provider_name):
+    """
+    Get json of ongoing outages data in "current_outages" folder in minio bucket.
+    """
+    # create uploader object
+    uploader = Uploader(bucket_name)
+
+    # specify local path to download file
+    local_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), 'current_outages.json'
+    )
+
+    # Download file if it exists, else create list to store new data to upload
+    try:
+        s3_path = f'{provider_name}/current_outages/current_outages.json'
+        uploader.download_file(s3_path, local_path)
+    except FileNotFoundError as e:
+        new_current_outages_list = []
+        with open('current_outages.json', 'w') as f:
+            json.dump(new_current_outages_list, f, ensure_ascii=False, indent=4)
+
+def upload_current_outages(current_outages: List[Dict[str, Any]], bucket_name, provider_name):
+    """
+    Save 'current_outages' as a json file then upload it to minio.
+    """
+
+    # save current outages
+    filename = 'current_outages.json'
+    with open(filename, 'w') as f:
+        json.dump(current_outages, f, ensure_ascii=False, indent=4)
+
+    # upload json to minio
+    uploader = Uploader(bucket_name)
+    local_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        'current_outages.json'
+    )
+    s3_path = f"{provider_name}/current_outages/{filename}"
+    uploader.upload_file(local_path, s3_path)
+
 if __name__ == "__main__":
 
+    # get datetime data once and pass it into functions as needed so it's consistent
+    now = datetime.now(timezone.utc)
+    current_datetime = {
+        'current_year': f"{now.year}",
+        'current_month': f"{now.month:02d}",
+        'current_day': f"{now.day:02d}",
+        'current_hour': f"{now.hour:02d}",
+        'current_minute': f"{now.minute:02d}",
+        'current_second': f"{now.second:02d}",
+    }
+
+    # get current day's raw data file from minio
+    pre_existing_raw_outages = get_pre_existing_raw_outages(current_datetime, bucket_name='spain', provider_name='edistribucion')
+
     # get data from api
-    new_data = get_data()
-    print(len(new_data))
+    new_raw_data = get_data()
+
+    # update pre-existing raw outage file with new data from api
+    updated_raw_data = update_raw_data(pre_existing_raw_outages, new_raw_data)
 
     # upload new raw data to minio
-    upload_raw(new_data)
+    upload_raw(updated_raw_data, current_datetime, bucket_name='spain', provider_name='edistribucion')
 
     # get pre-existing outages from minio
-    get_pre_existing_outages()
+    get_pre_existing_current_outages(bucket_name='spain', provider_name='edistribucion')
     with open('current_outages.json', 'r') as file:
         pre_existing_outages = json.load(file)
 
     # use new outage data to update pre_existing outages
-    updated_current_outages = update_current_outages(pre_existing_outages, new_data)
+    updated_current_outages = update_current_outages(pre_existing_outages, new_raw_data)
+
+    # upload updated current outages to mino
+    upload_current_outages(updated_current_outages, bucket_name='spain', provider_name='edistribucion')
---
+++ src/scrapers/spain/naturgy/notes.md
@@ -0,0 +1,182 @@
+## Data Retrieved
+
+OBJECTID - unique integer identifier for the outage event. helpful for tracking an individual outage across multiple 
+scraper runs.
+
+ZONA - (English: Area), string value
+
+TIPO_GRUPO - (English: Group Type), integer value
+
+ID_GRUPO - (English: Group ID), String value
+
+ID_GRUPO_INCI - (English: Groupd ID INCI?), Integer value
+
+PROVINCIA - (English: Province), Province of Spain where the outage is taking place.
+
+TIPO - (English: Type), integer value representing the category of the outage. (1: Planned, 2: Unplanned)
+
+CENTRO - (English: Center), integer value
+
+FECHA_DETECCION - (English: Detection Date), integer value representing when the outage was detected. Number of 
+milliseconds since 1970 in UTC time.
+
+FECHA_RESOLUCION - (English: Resolution Date), integer value representing when the outage is expected to be resolved. 
+Number of milliseconds since 1970 in UTC time.
+
+ESTADO - (English: State), string value representing current state of the outage. Seems like it's almost always 
+set to "En resolución" (in resolution).
+
+INS_AFECTADA - ?
+
+INS_ORIGEN - ?
+
+NIV_TEN - ?
+
+TIPO_INC - ?
+
+CLIENTES_AFECTADOS - (English: Affected Customers), integer value representing the number of clients affected 
+by the outage.
+
+DURACION - (English: Duration), Duration of the outage. All the outages being returned by this api are ongoing, so it 
+seems like this value is almost always set to null.
+
+created_date - integer value representing when the outage event was created. Number of milliseconds since 1970.
+
+last_edited_date - integer value representing when the outage event was last edited. Number of milliseconds since 1970. 
+All outages are updated together every 5 minutes. 
+
+FECHA_FICHERO - (English: File Date)
+
+CLIENTES_AFECTADOS_TXT - (English: Affected Customers Txt), string value representing the number of clients affected 
+by the outage.
+
+LastFile - integer value
+
+CENTRO_OPERACIONES - (English: Operations Center)
+
+## Data Retrieval
+
+This dataset comes from Naturgy, a power provider in spain, which provides live information on current and planned power outages in Spain.
+Data is accessed via the public api that is used to provide data to the live outage map on Naturgy's website.
+
+🔗 https://www.ufd.es/estado-del-servicio/
+
+The API endpoint used:
+
+https://gisoperaciones.ufd.es/server/rest/services/Averias/Avisos_VistaPublica/FeatureServer/0/query
+
+Parameters used:
+params = {
+            "f": "json",
+            "where": f"OBJECTID > {last_id}",
+            "outFields": "*",
+            "returnGeometry": "true",
+            "outSR": 4326,
+            "orderByFields": "OBJECTID ASC",
+            "resultRecordCount": 2000,
+        }
+
+Authentication:
+
+This dataset is publicly accessible and does not require an API key.
+However, requests should be rate-limited (e.g., 0.5–1.5 seconds between calls) to avoid throttling.
+
+API Response Structure:
+
+Each API response contains a JSON payload with an array of outage records in the features field. Each individual
+outage then also contains an attributes and a geometry field:
+
+{
+  "features": [
+    {
+        "attributes": {
+            "OBJECTID": 296041,
+            "ZONA": "N",
+            "TIPO_GRUPO": 0,
+            "ID_GRUPO": "611036415",
+            "ID_GRUPO_INCI": 0,
+            "PROVINCIA": "A Coruña",
+            "TIPO": 1,
+            "CENTRO": 6110,
+            "FECHA_DETECCION": 1764409800000,
+            "FECHA_RESOLUCION": -2209165200000,
+            "ESTADO": "En resolución",
+            "INS_AFECTADA": "1115:40208762",
+            "INS_ORIGEN": "",
+            "NIV_TEN": "B",
+            "TIPO_INC": "",
+            "CLIENTES_AFECTADOS": 83,
+            "DURACION": null,
+            "created_date": 1764412510000,
+            "last_edited_date": 1764436510000,
+            "FECHA_FICHERO": 1764436140000,
+            "CLIENTES_AFECTADOS_TXT": "83",
+            "LastFile": 1,
+            "CENTRO_OPERACIONES": "6110"
+        },
+        "geometry": {
+            "x": -8.376589294067683,
+            "y": 43.19723757667924
+        },
+    },
+    ...
+  ]
+}
+
+Code Process
+
+scrape.py:
+
+Requests records using public api. Stored as a list of dicts.
+
+Then pull the "current_outages.json" file from minio. This is a file that tracks currently ongoing outages. This is 
+helpful for outages that span multiple scraper runs so that each outage can be treated as a single event even 
+if it is seen multiple times. Once an outage has ended, it is then removed from this file and handled by the post 
+processor. 
+
+If an outage is already in the "current_outages.json" file and is still being seen by the api, then it is mostly left 
+unchanged, but if the number of clients increased in the newest api call then this value is updated in the 
+current outages file. 
+
+If an outage was just seen for the first time in the newest api call, then it is added to the 
+current outages file and given a tag "ioda_status" = "in_progress". This tag tells the post processor that this outage 
+is still ongoing and to ignore it for now. 
+
+And lastly, if an outage is contained in the current outage file but was not seen by the most recent api call, then 
+the outage has ended. This outage in the current outages file has its "ioda_status" tag updated to "resolved". This 
+tells the post processor that this outage has ended and the data can now be processed and removed from the current 
+outage file.
+
+After "current_outages.json" has been updated, it is then uploaded to minio (replacing its previous iteration).
+
+If this is the first time today that this scraper has been run, then the newly scraped data is downloaded locally to 
+the docker container as a json file then uploaded to minio. If the scraper has already been run today, then the raw 
+data file for current day is retrieved from minio, new raw outage data is added to file, then updated raw data file 
+is uploaded to minio. Since this scraper is run very frequently (every 4 minutes), condensing all the raw data to one 
+json file per day makes it more manageable to find the data you're looking for in minio.
+
+
+post_process.py:
+
+This script starts by downloading "current_outages.json" from minio (description above in scrape.py notes). It then 
+iterates through each of the outages in this file and checks the "ioda_status" value. An outage marked as "in_progress" 
+is still ongoing and is ignored. An outage marked as "resolved" is completed and can not be processed and removed 
+from the file.
+
+This script only stores one processed data file per day in minio. So if this file already exists in minio, then it 
+is retrieved so that we can be appended to. If it does not exist then we start with an empty list.
+
+Once we've iterated through all the outages in "current_outages.json" and pulled the appropriate data for post 
+processing. These outages are then run through a function that formats the outages for IODA, adds them to the list of 
+previously existing processed outages for today if there were any, then uploads the processed data file to minio
+(replacing any previous iteration of it).
+
+The updated "current_outages,json" file is also uploaded to minio. The resolved outages have been removed from it.
+
+## Other Notes
+
+The dataset updates approximately every 5 minutes.
+
+Outage categories include Planned (maintenance) and Unplanned (faults).
+
+Timestamps follow UTC (millisecond count since 1970).
